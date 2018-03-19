39b210a9f3b599c8365116627718fa1ac3add8fd
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-18 23:51:03 -0700

    Create farm_area.cpp

---
+++ farm_area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+double L,B,area;
+cout<<" Enter the lengthg and breadth of farm : "<<endl;
+cin>>L>>B;
+area=L*B;
+cout<<fixed<<setprecision(5)<<area<<endl;
+return 0;
+}
