321953a759a5708a3553ef2419ab4cf22557bb95
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-19 00:45:09 -0700

    Create calci.cpp

---
+++ calci.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n1,n2,n3,N;
+char op;
+cin>>n1>>op>>n2;
+if(op=='%')
+N=1;
+else if(op=='/')
+N=2;
+else
+N=0;
+switch(N)
+{ case 1:n3=n1%n2;
+           cout<<n3<<endl;
+           break;
+  case 2:n3=n1/n2;
+           cout<<n3<<endl;
+           break;
+  default:cout<<"Invalid operation"<<endl;
+}
+return 0;
+}
